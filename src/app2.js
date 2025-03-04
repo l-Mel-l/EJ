8e6241d882b85c498fc14e2c33f57a865efed9ad
Mel <constantine.dudorov@yandex.ru>
2025-03-04 16:52:35 +0300

    EJ

---
+++ src/app2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+import Clicker from './clicker';
+import './App2.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/clicker.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './Clicker.css';
+
+function Clicker() {
+  const [count, setCount] = useState(0);
+  const [instances, setInstances] = useState([]); // Массив объектов с данными для картинки и аудио
+
+  // Размер картинки (ширина)
+  const imgWidth = 80;
+  const imgHeight = 80;
+
+  // Функция для генерации случайной позиции с учетом размеров картинки
+  const getRandomPosition = () => {
+    const maxX = window.innerWidth - imgWidth-150;
+    const maxY = window.innerHeight - imgHeight-150;
+    const x = Math.floor(Math.random() * maxX);
+    const y = Math.floor(Math.random() * maxY);
+    return { x, y };
+  };
+
+  const handleClick = () => {
+    setCount(prev => prev + 1);
+    const { x, y } = getRandomPosition();
+
+    // Создаем уникальный id для картинки (например, с помощью времени)
+    const id = Date.now();
+    const newInstance = { id, x, y };
+
+    // Добавляем новый экземпляр в массив
+    setInstances(prev => [...prev, newInstance]);
+
+    // Через 2 секунды удаляем эту картинку
+    setTimeout(() => {
+        setInstances(prev => prev.filter(instance => instance.id !== id));
+      }, 5000);
+    };
+
+  const imageSrc = process.env.PUBLIC_URL + '/ej.jpg';
+  const audioSrc = process.env.PUBLIC_URL + '/chich.mp3';
+
+  return (
+    <div className="clicker-container">
+      <button className="clicker-button" onClick={handleClick}>
+        Кликни ежа!
+      </button>
+      <p className="clicker-count">{count} ежей</p>
+      {instances.map(instance => (
+        <div key={instance.id}>
+          <img src={imageSrc} alt="Random" className="clicker-image" style={{ left: instance.x, top: instance.y }}/>
+          <audio src={audioSrc} autoPlay style={{ display: 'none' }}
+            ref={audio => {if (audio) {audio.volume = 0.4;}
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+export default Clicker;
